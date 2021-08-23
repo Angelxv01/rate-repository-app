171c80e3b5c026715a768b6fe36d696f41142e0a
Angel <angeloxv@outlook.it>
2021-08-23 12:52:08 +0200

    part10: fix style issues with error

--- src/components/TextInput.jsx
+++ src/components/TextInput.jsx
@@ -1,9 +1,15 @@
 import React from "react";
-import { TextInput as NativeTextInput } from "react-native";
+import { TextInput as NativeTextInput, StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  errorBorder: {
+    borderColor: "#f55"
+  }
+});
 
 // eslint-disable-next-line no-unused-vars
 const TextInput = ({ style, error, ...props }) => {
-  const textInputStyle = [style];
+  const textInputStyle = error ? [style, styles.errorBorder] : [style];
 
   return <NativeTextInput style={textInputStyle} {...props} />;
 };
